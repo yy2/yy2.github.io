f998edc71d253509e47aec320b1fdf4c082edbda
Yu Yuan <yuzyuan93@gmail.com>
2018-08-05 17:55:37 -0400

    added js folder

---
+++ js/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+    <h1>Hello! Welcome!</h1>
+  );
+  }
+}
+
+ReactDOM.render('App', domument.querySelector("#app"));
