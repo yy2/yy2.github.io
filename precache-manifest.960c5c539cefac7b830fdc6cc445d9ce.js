eaa05f9c4a489e297d83c79fa2b18aa646e7c08f
Yu <yuzyuan93@gmail.com>
2020-11-29 19:59:58 -0500

    Updates

---
+++ precache-manifest.960c5c539cefac7b830fdc6cc445d9ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f033592fe4091d93f1ccbc5274801f06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "226deae8a19195068445",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "849d9d8a6bffe5fd4f90",
+    "url": "/static/css/main.7823e653.chunk.css"
+  },
+  {
+    "revision": "226deae8a19195068445",
+    "url": "/static/js/2.e13044ec.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.e13044ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "849d9d8a6bffe5fd4f90",
+    "url": "/static/js/main.84127919.chunk.js"
+  },
+  {
+    "revision": "45f70f642602627a775c",
+    "url": "/static/js/runtime-main.3e007ec9.js"
+  },
+  {
+    "revision": "57030944341251245c4cf5de338e0248",
+    "url": "/static/media/profile2.57030944.png"
+  }
+]);
